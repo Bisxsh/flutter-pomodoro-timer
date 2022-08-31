1b722cdcbdf9723e3576907ce9fe79a0ec16ff24
Bisesh <bisesh.sitaula@gmail.com>
2022-08-31 09:21:29 +0100

    Implemented play/pause button

--- lib/components/countdown_timer/widgets/timer_widget.dart
+++ lib/components/countdown_timer/widgets/timer_widget.dart
@@ -1,5 +1,6 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer_flutter/components/countdown_timer/widgets/play_pause_widget.dart';
 
 class TimerWidget extends StatefulWidget {
   const TimerWidget({super.key});
@@ -15,20 +16,32 @@ class _TimerWidgetState extends State<TimerWidget> {
 
   void toggleTimer() {
     if (!_controller.isStarted) {
-      _controller.start();
+      _controller.restart(duration: 30);
+
+      setState(() {
+        _controller.isStarted = true;
+        _controller.isResumed = true;
+      });
       return;
     } else if (_controller.isResumed) {
       _controller.pause();
-      _controller.isResumed = false;
+
+      setState(() {
+        _controller.isResumed = false;
+      });
     } else {
       _controller.resume();
-      _controller.isPaused = false;
+
+      setState(() {
+        _controller.isPaused = false;
+      });
     }
   }
 
   void resetTimer() {
-    _controller.restart(duration: _startingTime);
-    _controller.pause();
+    _controller.reset();
+    _controller.isPaused = false;
+    _controller.isResumed = false;
   }
 
   @override
@@ -44,23 +57,39 @@ class _TimerWidgetState extends State<TimerWidget> {
       child: GestureDetector(
         onTap: () => {toggleTimer()},
         onLongPress: () => {resetTimer()},
-        child: CircularCountDownTimer(
-          width: 250,
-          height: 250,
-          duration: 30,
-          fillColor: Colors.white,
-          ringColor: Colors.grey,
-          isReverse: true,
-          isReverseAnimation: true,
-          strokeWidth: 10.0,
-          strokeCap: StrokeCap.round,
-          textFormat: "mm:ss",
-          textStyle: const TextStyle(color: Colors.transparent),
-          onChange: (String timeStamp) {
-            timeRemaining = timeStamp;
-          },
-          controller: _controller,
-          autoStart: false,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            PlayPauseWidget(
+              showPlay: (!_controller.isStarted),
+              invisible: (_controller.isStarted && _controller.isResumed),
+            ),
+            CircularCountDownTimer(
+              width: 250,
+              height: 250,
+              duration: 30,
+              autoStart: false,
+              initialDuration: 30,
+              fillColor: Colors.white,
+              ringColor: Colors.grey,
+              isReverse: true,
+              isReverseAnimation: true,
+              strokeWidth: 10.0,
+              strokeCap: StrokeCap.round,
+              textFormat: "mm:ss",
+              textStyle: const TextStyle(color: Colors.transparent),
+              onChange: (String timeStamp) {
+                timeRemaining = timeStamp;
+              },
+              onComplete: () {
+                setState(() {
+                  _controller.reset();
+                  _controller.isStarted = false;
+                });
+              },
+              controller: _controller,
+            ),
+          ],
         ),
       ),
     );
