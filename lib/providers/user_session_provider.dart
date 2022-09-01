c2169a6931179ec48cee766125382002f0cc9241
Bisesh <bisesh.sitaula@gmail.com>
2022-09-01 13:15:04 +0100

    Implemented getters and setters for UserSession

--- lib/providers/user_session_provider.dart
+++ lib/providers/user_session_provider.dart
@@ -5,7 +5,26 @@ import 'package:flutter/material.dart';
 enum TimerMode { POMODORO, BREAK, LONG_BREAK }
 
 class UserSession with ChangeNotifier {
-  final String _timeRemaining = "";
-  final int _pomodorosCompleted = 0;
-  TimerMode mode = TimerMode.POMODORO;
+  String _timeRemaining = "";
+  int _pomodorosCompleted = 0;
+  TimerMode _mode = TimerMode.POMODORO;
+
+  String get timeRemaining => _timeRemaining;
+  int get pomodorosCompleted => _pomodorosCompleted;
+  TimerMode get mode => _mode;
+
+  set timeRemaining(String value) {
+    _timeRemaining = value;
+    notifyListeners();
+  }
+
+  set pomodorosCompleted(value) {
+    _pomodorosCompleted = value;
+    notifyListeners();
+  }
+
+  set mode(value) {
+    _mode = value;
+    notifyListeners();
+  }
 }
